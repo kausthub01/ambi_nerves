8f0c2cc37f813b4ce00c2ce103910bdcbbac821c
Kausthub Nandagiri <knandagiri@issi-software.com>
2025-07-25 19:32:16 -0400

    initial commit

--- universityofLumora/settings.py
+++ universityofLumora/settings.py
@@ -119,7 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+from pathlib import Path
+import os
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # ✅ this matches your structure
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
